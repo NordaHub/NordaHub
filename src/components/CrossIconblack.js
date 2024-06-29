a8623ebdb18218068675138b2cccced3f79f27e8
NordaHub <152313223+NordaHub@users.noreply.github.com>
2024-06-29 14:18:00 +0200

    Add files via upload

---
+++ src/components/CrossIconblack.js
@@ -0,0 +1,28 @@
+const CrossIcon24black = () => (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line
+        x1="1"
+        y1="1"
+        x2="23"
+        y2="23"
+        stroke="black"
+        strokeWidth="4"
+        strokeLinecap="round"
+      />
+      <line
+        x1="23"
+        y1="1"
+        x2="1"
+        y2="23"
+        stroke="black"
+        strokeWidth="4"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+  export default CrossIcon24black;
